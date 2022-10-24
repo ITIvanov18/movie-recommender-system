e2c1581c7ae2473e63d6cc81afc7804040a080e9
ITIvanov18 <ITIvanov18@codingburgas.bg>
2022-10-24 13:31:18 +0300

    Commit 02-7 Dependencies

---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson1/RecommenderImplementation.java
@@ -0,0 +1,14 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson1;
+
+public class RecommenderImplementation {
+    public String[] recommendMovies (String movie){
+
+        //user content based filter to find similar movies
+        ContentBasedFilter filter = new ContentBasedFilter();
+        String[] results = filter.getRecommendations("Finding Dory");
+
+        //return new String[] {"M1", "M2", "M3"};
+        return results;
+
+    }
+}
