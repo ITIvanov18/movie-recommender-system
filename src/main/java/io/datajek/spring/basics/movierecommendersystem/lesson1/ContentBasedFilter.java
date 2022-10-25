9e01a3ba3d3cb47c73a7531295a73156dbd0a110
Ivan Ivanov <59703243+ITIvanov18@users.noreply.github.com>
2022-10-25 14:43:55 +0300

    02-10 Component and Dependency Management

--- src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson1/ContentBasedFilter.java
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson1/ContentBasedFilter.java
@@ -1,7 +1,9 @@
 package io.datajek.spring.basics.movierecommendersystem.lesson1;
-
 import java.util.logging.Filter;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class ContentBasedFilter implements Filter {
     public String[] getRecommendations(String  movie){
 
